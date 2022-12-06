187907963b28cd5063f5ac7e1c754c4c2ef6d251
Thành <37612705+becamegod@users.noreply.github.com>
2022-12-06 21:58:24 +0700

    Fix presentation's default timestamp

--- src/components/presentation/presentation.service.js
+++ src/components/presentation/presentation.service.js
@@ -17,15 +17,20 @@ const PresentationService = {
   },
 
   async list(groupId) {
-    const presentations = await Presentation.find({ groupId }).lean();
+    const transform = (doc, id) => (doc == null) ? "Unknown" : doc.name;
+    const presentations = await Presentation.find({ groupId })
+      .populate([{path: "owner", transform}]).lean();
     return presentations;
   },
 
   async create(name, owner, group) {
+    const time = new Date();
     const newPresentation = await Presentation({
       name,
       owner,
       group,
+      created: time,
+      modified: time
     });
     return newPresentation.save();
   },
