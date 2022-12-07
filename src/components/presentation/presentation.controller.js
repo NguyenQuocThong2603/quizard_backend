c2d53504edfa8f1ff365d8a06d6e980c970c3054
nguyenquocthong2603 <nguyenquocthong2603@gmail.com>
2022-12-07 18:09:01 +0700

    get detail of presentation

--- src/components/presentation/presentation.controller.js
+++ src/components/presentation/presentation.controller.js
@@ -33,29 +33,11 @@ const PresentationController = {
   async getDetail(req, res) {
     const { presentationId } = req.params;
     try {
-      const presentation = await PresentationService.findPresentationById(presentationId);
+      const presentation = await PresentationService.find(presentationId);
       if (!presentation) {
         return res.status(statusCode.NOT_FOUND).json({ message: 'Presentation not found' });
       }
-      const users = await UserService.findAllUsersInPresentation(presentation._id);
-
-      users.forEach(user => {
-        if (user.email === presentation.owner) {
-          user.role = 'Owner';
-        } else if ((_.findIndex(presentation.roles, role => role === user.email) !== -1)) {
-          user.role = 'Co-Owner';
-        } else {
-          user.role = 'Member';
-        }
-      });
-      const presentationDTO = {
-        _id: presentation._id,
-        presentationId: presentation.presentationId,
-        name: presentation.name,
-        description: presentation.description,
-        joinedUser: users,
-      };
-      return res.status(statusCode.OK).json(presentationDTO);
+      return res.status(statusCode.OK).json({ message: 'Get detail successfully', presentation });
     } catch (err) {
       return res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message: 'Internal Server Error' });
     }
